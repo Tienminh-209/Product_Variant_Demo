26574a83b25dd824d2b72ec3b1f758db722ea5b7
Nguyen Ngoc Bao Trong <nnbaotrong@gmail.com>
2022-06-02 09:34:33 +0700

    build model

---
+++ src/main/java/com/example/demo/model/Skus.java
@@ -0,0 +1,48 @@
+package com.example.demo.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "skus")
+public class Skus {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "sku")
+	private String sku;
+
+	@Column(name = "price")
+	private double price;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getSku() {
+		return sku;
+	}
+
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+}
