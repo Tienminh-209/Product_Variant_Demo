1702edce45ba80d65d31067b4bbf322e1997f785
Tienminh209 <buiminhtien2025@gmail.com>
2022-06-02 10:21:59 +0700

    c4 add file ProductVariantsValue.java and ProductVariantsOptions.java

--- src/main/java/com/example/demo/model/ProductVariantsOptions.java
+++ src/main/java/com/example/demo/model/ProductVariantsOptions.java
@@ -1,9 +1,15 @@
 package com.example.demo.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -13,8 +19,21 @@ public class ProductVariantsOptions {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+	@Column(name = "name")
 	private String name;
+
+	public ProductVariantsOptions() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@OneToMany(mappedBy = "variantsOptions",cascade = CascadeType.ALL)
+	Set<ProductVariantsValue> productVariantsValues = new HashSet<ProductVariantsValue>();
+
+	public ProductVariantsOptions(Long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
 
 	public Long getId() {
 		return id;
@@ -31,5 +50,13 @@ public class ProductVariantsOptions {
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
+	public Set<ProductVariantsValue> getProductVariantsValues() {
+		return productVariantsValues;
+	}
+
+	public void setProductVariantsValues(Set<ProductVariantsValue> productVariantsValues) {
+		this.productVariantsValues = productVariantsValues;
+	}
+
 }
--- src/main/java/com/example/demo/model/ProductVariantsValue.java
+++ src/main/java/com/example/demo/model/ProductVariantsValue.java
@@ -1,20 +1,42 @@
 package com.example.demo.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 @Entity
 @Table(name = "product_variants_values")
 public class ProductVariantsValue {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+	@Column(name = "name")
 	private String name;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "category_id")
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	private ProductVariantsOptions variantsOptions;
+
+	public ProductVariantsValue() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public ProductVariantsValue(Long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
 
 	public Long getId() {
 		return id;
@@ -30,6 +52,14 @@ public class ProductVariantsValue {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public ProductVariantsOptions getVariantsOptions() {
+		return variantsOptions;
+	}
+
+	public void setVariantsOptions(ProductVariantsOptions variantsOptions) {
+		this.variantsOptions = variantsOptions;
 	}
 
 }
